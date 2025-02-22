7204e19645b6d642cbf7c92ecfe53134e8f420fe
BlueSugar <emerald620blue@gmail.com>
2025-02-22 09:33:37 +0900

    add disjoint set union

---
+++ data_structure/src/unionfind.rs
@@ -0,0 +1,70 @@
+use std::io::Write;
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    let mut stdin = stdin.split_whitespace();
+    let mut stdout = std::io::BufWriter::new(std::io::stdout().lock());
+
+    let n: usize = stdin.next().unwrap().parse().unwrap();
+    let q: usize = stdin.next().unwrap().parse().unwrap();
+
+    let mut dsu = DisjointSetUnion::new(n);
+    for _ in 0..q {
+        let t: u8 = stdin.next().unwrap().parse().unwrap();
+        let u: usize = stdin.next().unwrap().parse().unwrap();
+        let v: usize = stdin.next().unwrap().parse().unwrap();
+
+        if t == 0 {
+            dsu.unite(u, v);
+        } else {
+            writeln!(stdout, "{}", if dsu.is_same(u, v) { 1 } else { 0 }).ok();
+        }
+    }
+}
+
+pub struct DisjointSetUnion {
+    parents: Vec<isize>,
+    cnt: usize,
+}
+
+impl DisjointSetUnion {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parents: vec![-1; n],
+            cnt: n,
+        }
+    }
+
+    pub fn root(&self, mut v: usize) -> usize {
+        while self.parents[v] >= 0 {
+            v = self.parents[v] as usize;
+        }
+        v
+    }
+
+    pub fn unite(&mut self, u: usize, v: usize) {
+        let mut u = self.root(u);
+        let mut v = self.root(v);
+        if u == v {
+            return;
+        }
+        if self.parents[u] > self.parents[v] {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.parents[u] += self.parents[v];
+        self.parents[v] = u as isize;
+        self.cnt -= 1;
+    }
+
+    pub fn is_same(&self, u: usize, v: usize) -> bool {
+        self.root(u) == self.root(v)
+    }
+
+    pub fn size(&self, v: usize) -> usize {
+        -self.parents[self.root(v)] as usize
+    }
+
+    pub fn cnt(&self) -> usize {
+        self.cnt
+    }
+}
